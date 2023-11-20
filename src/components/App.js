7ecc7fd9a4d9c0aa901a3dd0cba859a62df1052d
yhan4 <yhan.4@hotmail.com>
2023-11-19 17:45:29 -0800

    Step 1: Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import '../styles/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>Booklist</header>
+      <p>Users can add and remove books from their book list.</p>
+    </div>
+  );
+}
+
+export default App;
