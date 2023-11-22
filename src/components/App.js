9ad9ba49e268eea8952ed2140a1a6231b4a58ae2
yhan4 <yhan.4@hotmail.com>
2023-11-21 21:45:37 -0800

    Step 3: Introducing Props

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,19 @@ import '../styles/App.css';
 import BookList from './BookList'
 
 function App() {
+  const books = [
+    { id: 1, title: 'Book 1', author: 'Author 1' },
+    { id: 2, title: 'Book 2', author: 'Author 2' },
+    { id: 3, title: 'Book 3', author: 'Author 3' },
+  ];
+
   return (
     <div className="App">
        <header className="App-header">
         <h1>Book List App</h1>
         <p>This is a simple app to manage your book list.</p>
       </header>
-      <BookList />
+      <BookList books = {books} />
     </div>
   );
 }
--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,10 +1,4 @@
-function BookList () {
-    const books = [
-        {id: 1, title: 'Book 1', author: 'Author 1'},
-        {id: 2, title: 'Book 2', author: 'Author 2'},
-        {id: 3, title: 'Book 3', author: 'Author 3'},
-    ];
-
+function BookList ({books}) {
     return (
         <div>
             <ul>
